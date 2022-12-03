89361e6b72df3f7a4b70c6fb7a306de7d09e7cf4
Tom Early <n7tae@tearly.net>
2022-12-03 10:18:22 -0700

    /etc/systemd/sytem/.service is always created from systemd/mrefd.service

--- example.mk
+++ example.mk
@@ -2,17 +2,17 @@
 
 # This is for the "install" target:
 # If you don't like to use symbolic links for the mrefd.{cfg,blacklist,whitelist,interlink}
-# files and you want to copy instead of link set this to "false"
+# files and you want to copy instead of link set this to "false".
 USESYMLINK = true
 
-# This is the folder where the executable file will be install.
+# This is the folder where the executable file will be installed.
 BINDIR = /usr/local/bin
 
-# this is the folder where config files (or symbolic links) are located
+# This is the folder where config files (or symbolic links) are located.
 CFGDIR = /usr/local/etc
 
-# for debugging support, set DEBUG to true
+# For debugging support, set DEBUG to true.
 DEBUG = false
 
-# to disable DHT support, set DHT to false
+# To disable DHT support, set DHT to false.
 DHT = true
