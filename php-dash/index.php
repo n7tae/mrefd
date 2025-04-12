9823d58ef28ea56be7a89cd8251b339a4411ff9c
Colby Ross <colbypr@gmail.com>
2025-04-12 16:51:05 -0400

    Fix M17 logo

--- php-dash/index.php
+++ php-dash/index.php
@@ -100,9 +100,7 @@ $Reflector->LoadXML();
     include_once("tracking.php");
 } ?>
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-    <nav class="navbar-circular-container">
-    <img style="background-color:white;" src="images/icons/favicon-96x96.png" class="navbar-circular-image">
-    </img></nav>&nbsp;&nbsp;
+    <img style="background-color: white; border-radius: 50%; height: 42px; width: 42px;" src="images/icons/round-logo.png"></img>&nbsp;&nbsp;
     <a class="navbar-brand" href="#"><?php echo $Reflector->GetReflectorName(); ?> Reflector</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
